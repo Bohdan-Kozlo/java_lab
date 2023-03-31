9e5751cde58eefbe664ff520c8517ba8668bff4c
Bohdan-Kozlo <119170585+Bohdan-Kozlo@users.noreply.github.com>
2023-03-31 22:32:09 +0300

    adding java code

---
+++ ua/lviv/iot/algo/part1/lab2/ChairManager.java
@@ -0,0 +1,48 @@
+package ua.lviv.iot.algo.part1.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ChairManager {
+    private final List<Chair> chairs;
+
+    public ChairManager() {
+        chairs = new ArrayList<>();
+    }
+
+    public void addChair(Chair chair) {
+        chairs.add(chair);
+    }
+
+    public List<Chair> searchByMaterial(String material) {
+        return chairs.stream()
+                .filter(c -> c.getMaterial() != null && c.getMaterial().equals(material))
+                .collect(Collectors.toList());
+    }
+
+    public List<Chair> searchByMaxWeight(int maxWeight) {
+        return chairs.stream()
+                .filter(c -> c.getMaxWeight() >= maxWeight)
+                .collect(Collectors.toList());
+    }
+
+
+    public static void main(String[] args) {
+        ChairManager manager = new ChairManager();
+        manager.addChair(new FeedingTable(1, "wood", 100, 40, "John", 2));
+        manager.addChair(new FeedingTable(2, "plastic", 50, 30, "Mary", 1));
+        manager.addChair(new GameChair(1, "leather", 120, "Alex", 25, 100, 15));
+        manager.addChair(new GameChair(2, "mesh", 80, "Kate", 20, 95, 10));
+        manager.addChair(new OfficeChair(1, "leather", 120, "Alex", "executive", "leather", 45.0));
+        manager.addChair(new OfficeChair(2, "mesh", 80, "Kate", "task", "fabric", 30.0));
+        manager.addChair(new SoftChair(1, "suede", 150, "John", "foam", 60, true));
+        manager.addChair(new SoftChair(2, "leather", 200, "Kate", "polyester", 70, false));
+
+        System.out.println(manager.searchByMaxWeight(100));
+        System.out.println(manager.searchByMaterial("leather"));
+
+
+
+    }
+}
---
+++ ua/lviv/iot/algo/part1/lab2/FeedingTable.java
@@ -0,0 +1,40 @@
+package ua.lviv.iot.algo.part1.lab2;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FeedingTable extends Chair {
+    private int maxWithChair;
+    private int minWithChair;
+    private int ageChild;
+
+    public FeedingTable(int id,
+                        String material,
+                        int maxWeightCHAIR,
+                        int minWithChair,
+                        String owner,
+                        int ageChild) {
+        super(id, material, maxWeightCHAIR, owner);
+        this.minWithChair = minWithChair;
+        this.minWithChair = maxWeightCHAIR;
+        this.ageChild = ageChild;
+    }
+
+
+    @Override
+    public void adjustPosition(int value) {
+        maxWithChair += value;
+        minWithChair += value;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", maxWithChair:" + minWithChair + " , minWithChair: " + minWithChair + " ,age Child: " + ageChild;
+    }
+
+
+}
